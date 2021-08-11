528e05db35ac908a89a38883d57c3c555e668170
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-08-11 18:43:45 -0300

    removendo arquivo de teste

--- teste.py
+++
@@ -1,9 +0,0 @@
-import os
-
-pasta = os.path.join(os.getcwd(), 'RPA_Selenium')
-
-[print(arquivos) for diretorio, subpastas, arquivos in os.walk(pasta)]
-
-# for diretorio, subpastas, arquivos in os.walk(pasta):
-#     for arquivo in arquivos:
-#         print(os.path.join(diretorio, arquivo))
